5f6858c4b76406c00906a841790601e8e3144726
Yashwant Tailor <yasht88101@gmail.com>
2021-03-06 00:44:10 +0530

    Python Vibrate Circle

---
+++ Star/Star1.py
@@ -0,0 +1,30 @@
+import turtle
+import random
+
+list1 = [True , False  ] 
+
+t = turtle.Turtle()
+s = turtle.Screen()
+
+s.bgcolor( 'black')
+t.pencolor('red')
+
+a = 0
+b = 0 
+t.speed(6)
+t.penup()
+t.goto(0 , 200)
+t.pendown()
+while( True ):
+    t.forward(a)
+    t.right(b)
+    a += 3
+    b += 1
+    if( random.choice(list1)):
+        t.pencolor('yellow')
+    else:
+        t.pencolor('red')
+    if b == 210 :
+        break
+    t.hideturtle()
+turtle.done()
